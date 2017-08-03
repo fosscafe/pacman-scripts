c44e5401c8eb55c638a0d112676932a5c76e97ba
Saifi Khan <saifi@strikr.in>
2017-08-03 10:28:37 +0530

    network tools group install script

---
+++ do_install_pacman_net_tools.sh
@@ -0,0 +1,20 @@
+pacman -S tcpdump         \
+		  bridge-utils    \
+		  util-linux      \
+		  dnsutils        \
+		  ldns            \
+		  iptables        \
+		  iputils         \
+		  iproute2        \
+		  iftop           \
+		  apache          \
+		  strace          \
+		  curl            \
+		  ethtool         \
+		  ipvsadm         \
+		  ngrep           \
+		  iperf           \
+		  nmap            \
+		  conntrack-tools \
+		  socat           \
+		  openbsd-netcat 
